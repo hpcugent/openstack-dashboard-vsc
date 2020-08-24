1261142e38f03060ee6dd8c8bfac65014be34a2a
Álvaro Simón <varosim@gmail.com>
2020-08-24 10:04:07 +0200

    Disable rh theme in tripleo

---
+++ files/panels/_12_vsc_theme.py
@@ -0,0 +1,7 @@
+
+# Disable RH theme and set VSC links
+
+AVAILABLE_THEMES = [('default', 'VSC Theme', 'themes/default')]
+SITE_BRANDING = "VSC-TIER1 Cloud"
+SITE_BRANDING_LINK = "https://www.vscentrum.be/"
+
